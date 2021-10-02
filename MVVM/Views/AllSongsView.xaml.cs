4062e7a3029feecc901716de0e6c91503fe82e5e
gormrgryn <gor-margaryan-05@mail.ru>
2021-10-02 21:18:59 +0400

    added thumb move to point function

--- MVVM/Views/AllSongsView.xaml.cs
+++ MVVM/Views/AllSongsView.xaml.cs
@@ -25,12 +25,28 @@ namespace MusicPlayerWPF.MVVM.Views
         public AllSongsView()
         {
             InitializeComponent();
+
+            SongSlider.ApplyTemplate();
+            Thumb thumb = (SongSlider.Template.FindName("PART_Track", SongSlider) as Track).Thumb;
+            thumb.MouseEnter += new MouseEventHandler(thumb_MouseEnter);
         }
+        
         private void Slider_DragCompleted(object sender, DragCompletedEventArgs e)
         {
             var vm = this.DataContext as AllSongsViewModel;
             vm.model.CurrentSong.Player.Position = TimeSpan.FromSeconds(SongSlider.Value);
             vm.CurrentSong = vm.model.CurrentSong;
+        }
+
+        private void thumb_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed && e.MouseDevice.Captured == null)
+            {
+                MouseButtonEventArgs args =
+                    new MouseButtonEventArgs(e.MouseDevice, e.Timestamp, MouseButton.Left);
+                args.RoutedEvent = MouseLeftButtonDownEvent;
+                (sender as Thumb).RaiseEvent(args);
+            }
         }
     }
 }
